c577b3f2ceef37f8c274a83b70b240d0227b8556
Sylvain Tran <sylvain.l.tran@gmail.com>
2020-06-09 08:28:47 -0400

    Moved to external, added tp2 base dir

---
+++ tp2/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Travail Pratique #1</title>
+    </head>
+    <body>
+        <h1>Anim&es et Mangas</h1>
+        <h2 id="header__date">Le Lundi 14 septembre 2009</h2>
+        <hr>
+        <p>
+            Travail fait par: Sylvain Tran p1195417
+        </p>
+    </body>
+</html>
