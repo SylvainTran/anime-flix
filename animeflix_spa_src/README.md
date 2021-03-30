afe9f099105883b49152f0da9634f8ba1c54841c
Sylvain Tran <sylvain.l.tran@gmail.com>
2021-03-30 11:52:58 -0400

    Update repos

---
+++ animeflix_spa_src/README.md
@@ -0,0 +1,8 @@
+# tp3
+
+## Todo:
+-̶S̶a̶m̶e̶ i̶n̶d̶e̶x̶.̶p̶h̶p̶
+-̶O̶O̶P̶
+-̶A̶j̶a̶x̶ s̶t̶u̶f̶f̶
+-̶R̶e̶m̶o̶v̶e̶ c̶o̶n̶n̶e̶c̶t̶i̶o̶n̶/̶d̶e̶c̶o̶n̶n̶e̶c̶t̶i̶o̶n̶
+-̶J̶S̶O̶N̶
--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(setup);
-
-function setup() {
-    $('#button main__button--display-weather').on("click", ()=> {
-        $('#city-select-form').submit();
-    });
-}
